9d190b4509fd40896054b0d28d9654862a711b3d
Marius Knaust <marius.knaust@gmail.com>
2018-06-12 09:38:03 +0200

    Added check for checksums

--- spotlight.sh
+++ spotlight.sh
@@ -21,12 +21,20 @@ function setImage
 	item=$(jq -r ".batchrsp.items[0].item" <<< $response)
 
 	landscapeUrl=$(jq -r ".ad.image_fullscreen_001_landscape.u" <<< $item)
+	sha256=$(jq -r ".ad.image_fullscreen_001_landscape.sha256" <<< $item | base64 -d | hexdump -ve "1/1 \"%.2x\"")
 	title=$(jq -r ".ad.title_text.tx" <<< $item)
 	searchTerms=$(jq -r ".ad.title_destination_url.u" <<< $item | perl -pe 's/.*?q=(.*?)&.*/\1/' | decodeURL)
 
 	path="$HOME/.spotlight/$name.jpg"
 
 	wget -qO "$path" "$landscapeUrl"
+	sha256calculated=$(sha256sum $path | cut -d " " -f 1)
+
+	if [ "$sha256" != "$sha256calculated" ]
+	then
+		systemd-cat -t spotlight -p emerg <<< "Checksum for $name incorrect"
+		exit 1
+	fi
 
 	gsettings set "org.gnome.desktop.$name" picture-options "zoom"
 	gsettings set "org.gnome.desktop.$name" picture-uri "file://$path"
