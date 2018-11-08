ea30c82dc4df99db5d29200ca731d8fcfa03ecce
Fabian Grutschus <fabian.grutschus@invitel-ug.de>
2018-11-08 11:26:17 +0100

    Added support for Cinnamon and only get image only once

--- spotlight.sh
+++ spotlight.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 dataPath="${XDG_DATA_HOME:-$HOME/.local/share}/spotlight"
 
@@ -7,40 +7,51 @@ function decodeURL
 	printf "%b\n" "$(sed 's/+/ /g; s/%\([0-9A-F][0-9A-F]\)/\\x\1/g')"
 }
 
-function setImage
+function getImage
 {
-	name="$1"
+        response=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=en,en-US&ctry=US")
+        status=$?
 
-	response=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=en,en-US&ctry=US")
-	status=$?
-
-	if [ $status -ne 0 ]
-	then
-		systemd-cat -t spotlight -p emerg <<< "Query for $name failed"
-		exit $status
-	fi
+        if [ $status -ne 0 ]
+        then
+            	systemd-cat -t spotlight -p emerg <<< "Query for $name failed"
+                exit $status
+        fi
 
 	item=$(jq -r ".batchrsp.items[0].item" <<< $response)
 
-	landscapeUrl=$(jq -r ".ad.image_fullscreen_001_landscape.u" <<< $item)
-	sha256=$(jq -r ".ad.image_fullscreen_001_landscape.sha256" <<< $item | base64 -d | hexdump -ve "1/1 \"%.2x\"")
-	title=$(jq -r ".ad.title_text.tx" <<< $item)
-	searchTerms=$(jq -r ".ad.title_destination_url.u" <<< $item | perl -pe 's/.*?q=(.*?)&.*/\1/' | decodeURL)
+        landscapeUrl=$(jq -r ".ad.image_fullscreen_001_landscape.u" <<< $item)
+        sha256=$(jq -r ".ad.image_fullscreen_001_landscape.sha256" <<< $item | base64 -d | hexdump -ve "1/1 \"%.2x\"")
+        title=$(jq -r ".ad.title_text.tx" <<< $item)
+        searchTerms=$(jq -r ".ad.title_destination_url.u" <<< $item | perl -pe 's/.*?q=(.*?)&.*/\1/' | decodeURL)
 
-	mkdir -p "$dataPath"
-	path="$dataPath/$name.jpg"
+        mkdir -p "$dataPath"
+        path="$dataPath/$name.jpg"
 
-	wget -qO "$path" "$landscapeUrl"
-	sha256calculated=$(sha256sum $path | cut -d " " -f 1)
+        wget -qO "$path" "$landscapeUrl"
+        sha256calculated=$(sha256sum $path | cut -d " " -f 1)
 
-	if [ "$sha256" != "$sha256calculated" ]
-	then
-		systemd-cat -t spotlight -p emerg <<< "Checksum for $name incorrect"
+        if [ "$sha256" != "$sha256calculated" ]
+        then
+            	systemd-cat -t spotlight -p emerg <<< "Checksum for $name incorrect"
+                exit 1
+        fi
+}
+
+function setImage
+{
+	local name="$1"
+
+	if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+		gsettings set "org.gnome.desktop.$name" picture-options "zoom"
+		gsettings set "org.gnome.desktop.$name" picture-uri "file://$path"
+	elif [ "$XDG_CURRENT_DESKTOP" = "X-Cinnamon" ]; then
+		gsettings set "org.cinnamon.desktop.$name" picture-options "zoom"
+                gsettings set "org.cinnamon.desktop.$name" picture-uri "file://$path"
+	else
+		echo "Unsupported desktop envoironment"
 		exit 1
 	fi
-
-	gsettings set "org.gnome.desktop.$name" picture-options "zoom"
-	gsettings set "org.gnome.desktop.$name" picture-uri "file://$path"
 
 	capitalName="$(tr '[:lower:]' '[:upper:]' <<< ${name:0:1})${name:1}"
 
@@ -48,5 +59,8 @@ function setImage
 	systemd-cat -t spotlight -p info <<< "$capitalName changed to $title ($searchTerms)"
 }
 
+getImage
 setImage "background"
-setImage "screensaver"
+if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+	setImage "screensaver"
+fi
