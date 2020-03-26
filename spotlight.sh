11c5de81437f2e1064273aa2b4248bf034867066
dpellegr <pellegrini.dario@gmail.com>
2020-03-26 19:03:36 +0100

    added options for custom path and save img

--- spotlight.sh
+++ spotlight.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
 dataPath="${XDG_DATA_HOME:-$HOME/.local/share}/spotlight"
+store=false
+
+while getopts ":hp:s" opt; do
+  case ${opt} in
+    h ) echo "spotlight.sh - Windows 10 Spotlight Background images for Gnome"
+        echo ""
+        echo "Options:"
+        echo "  -h shows this message"
+        echo "  -p specifies a working path. Defaults to \"$HOME/.local/share/spotlight\""
+        echo "  -s stores the images at path/archive/"
+      ;;
+    p ) dataPath=$OPTARG
+      ;;
+    s ) store=true
+      ;;
+    \? ) echo "Usage: spotlight.sh [-p working path] [-s save images]"
+      ;;
+  esac
+done
 
 function decodeURL
 {
@@ -21,13 +40,13 @@ item=$(jq -r ".batchrsp.items[0].item" <<< $response)
 landscapeUrl=$(jq -r ".ad.image_fullscreen_001_landscape.u" <<< $item)
 sha256=$(jq -r ".ad.image_fullscreen_001_landscape.sha256" <<< $item | base64 -d | hexdump -ve "1/1 \"%.2x\"")
 title=$(jq -r ".ad.title_text.tx" <<< $item)
-searchTerms=$(jq -r ".ad.title_destination_url.u" <<< $item | sed 's/.*q=\([^&]*\).*/\1/' | decodeURL)
+searchTerms=$(jq -r ".ad.title_destination_url.u" <<< $item | perl -pe 's/.*?q=(.*?)&.*/\1/' | decodeURL)
 
 mkdir -p "$dataPath"
-path="$dataPath/background.jpg"
+img="$dataPath/current_background.jpg"
 
-wget -qO "$path" "$landscapeUrl"
-sha256calculated=$(sha256sum $path | cut -d " " -f 1)
+wget -qO "$img" "$landscapeUrl"
+sha256calculated=$(sha256sum $img | cut -d " " -f 1)
 
 if [ "$sha256" != "$sha256calculated" ]
 then
@@ -35,8 +54,14 @@ then
 	exit 1
 fi
 
+if [ "$save" = true ] 
+then
+        mkdir -p "$dataPath/archive"
+	cp "$img" "$dataPath/archive/$(date +%Y%m%d) $title ($searchTerms).jpg"
+fi
+
 gsettings set "org.gnome.desktop.background" picture-options "zoom"
-gsettings set "org.gnome.desktop.background" picture-uri "file://$path"
+gsettings set "org.gnome.desktop.background" picture-uri "file://$img"
 
 notify-send "Background changed" "$title ($searchTerms)" --icon=preferences-desktop-wallpaper --urgency=low #--hint=string:desktop-entry:spotlight
 systemd-cat -t spotlight -p info <<< "Background changed to $title ($searchTerms)"
